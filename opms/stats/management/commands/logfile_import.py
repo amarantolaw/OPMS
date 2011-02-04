331f7af5298363a89e81b5619ad0df6cdad4c1d9
Carl Marshall <carl.marshall@oucs.ox.ac.uk>
2011-02-04 19:10:31 +0000

    devolving methods for foreign key objects

--- opms/stats/management/commands/logfile_import.py
+++ opms/stats/management/commands/logfile_import.py
@@ -12,13 +12,6 @@ class Command(BaseCommand):
     help = 'Imports the contents of the specified spreadsheet into the database'
 
     def handle(self, *args, **options):
-        
-# Locate file
-# Open file
-# Read line by line
-#  parse line
-#  store in relevant fields
-#  do associated lookups and stores
 
         for filename in args:
             # Some basic checking
@@ -38,6 +31,18 @@ class Command(BaseCommand):
                 data = p.parse(line)
                 
                 # Validate the data - Count the number of elements
+                if len(data) <> 11:
+                    print "#### Houston, we have a problem with this entry: %s" % data
+                
+                
+                # Get or create the foreign key elements, Logfile, Rdns, FileRequest, Referer, UserAgent
+                logfile = self._logfile(filename)
+                remote_rdns = self._ip_to_domainname(data.get('%h'))
+                file_request = self._file_request(data.get('%r'))
+                referer = self._referer(data.get('%{Referer}i'))
+                user_agent = self._user_agent(data.get('%{User-Agent}i'))
+                
+                # Tracking needs dealing with later...
                 
                 # Pull apart the date time string
                 date_string, time_string = data.get('%Y-%m-%dT%H:%M:%S%z').split('T')
@@ -57,8 +62,9 @@ class Command(BaseCommand):
                 # Status code validation
                 status_code = int(data.get('%>s'))
                 
+                # Build the log entry dictionary
                 log_entry = {
-                    'logfile': filename,
+                    'logfile': logfile,
                     'time_of_request': datetime.datetime(
                         int(date_yyyy), 
                         int(date_mm), 
@@ -73,17 +79,26 @@ class Command(BaseCommand):
                     'remote_ip': data.get('%h'),
                     'remote_logname': data.get('%l'),
                     'remote_user': data.get('%u'),
-                    'remote_rdns': self._ip_to_domainname(data.get('%h')),
+                    'remote_rdns': remote_rdns,
                     'status_code': status_code,
                     'size_of_response': size_of_response,
-                    'file_request': data.get('%r'),
-                    'referer': data.get('%{Referer}i'),
-                    'user_agent': data.get('%{User-Agent}i'),
+                    'file_request': file_request,
+                    'referer': referer,
+                    'user_agent': user_agent,
                 }
                 
                 print '============================\n'
                 print data,'\n'
                 print log_entry,'\n'
+
+
+        
+# Locate file
+# Open file
+# Read line by line
+#  parse line
+#  store in relevant fields
+#  do associated lookups and stores
                 
 # data.items()    
 #[('%Y-%m-%dT%H:%M:%S%z', '2009-01-14T06:20:59+0000'),
@@ -105,10 +120,37 @@ class Command(BaseCommand):
             print "Import finished\n\n"
 
 
+    def _logfile(self, filename):
+        "Get or create a LogFile record for the given filename"
+        return filename
+
 # Reverse DNS====
 # from dns import resolver,reversename
 # addr=reversename.from_address("192.168.0.1")
 # str(resolver.query(addr,"PTR")[0])
 
     def _ip_to_domainname(self, ipaddress):
+        "Returns the domain name for a given IP where known"
         return "www.%s.com" % ipaddress
+
+    def _file_request(self, request_string):
+        "Get or create a FileRequest object for a given request string"
+        return request_string
+        
+    def _referer(self, referer_string):
+        "Get or create a Referer record for the given string"
+        return referer_string
+        
+    def _user_agent(self, agent_string):
+        "Get or create a UserAgent record for the given string"
+        # Split the string into likely parts
+        # ua, created = UserAgent.objects.get_or_create(field=value, field=value, defaults=everything)
+
+        # obj, created = Preview.objects.get_or_create(week_ending=report.get('week_ending'), handle=report.get('handle'), defaults=report)
+            
+        # if created:
+        #     obj.save()
+        # else:
+
+        return agent_string
+        
