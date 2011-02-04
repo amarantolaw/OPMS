a61cab5554e196c3b1c9b129777dae8dd6d918fe
Carl Marshall <carl.marshall@oucs.ox.ac.uk>
2011-02-04 17:56:29 +0000

    Changing the comment formatting

--- opms/stats/management/commands/logfile_import.py
+++ opms/stats/management/commands/logfile_import.py
@@ -12,14 +12,14 @@ class Command(BaseCommand):
     help = 'Imports the contents of the specified spreadsheet into the database'
 
     def handle(self, *args, **options):
-'''        
-Locate file
-Open file
-Read line by line
-  parse line
-  store in relevant fields
-  do associated lookups and stores
-'''
+        
+# Locate file
+# Open file
+# Read line by line
+#  parse line
+#  store in relevant fields
+#  do associated lookups and stores
+
         for filename in args:
             # Some basic checking
             if filename.endswith('.gz'):
@@ -37,32 +37,30 @@ Read line by line
             for line in log:
                 print p.parse(line)
                 
-'''
-data.items()    
-[('%Y-%m-%dT%H:%M:%S%z', '2009-01-14T06:20:59+0000'),
- ('%l', '-'),
- ('%>s', '200'),
- ('%h', '163.1.2.86'),
- ('%A:%p', '163.1.3.25:80'),
- ('%{User-Agent}i', 'check_http/1.96 (nagios-plugins 1.4.5)'),
- ('%b', '207'),
- ('%{Referer}i', '-'),
- ('%u', '-'),
- ('%v', 'media.podcasts.ox.ac.uk'),
- ('%r', 'GET / HTTP/1.0')]
+# data.items()    
+#[('%Y-%m-%dT%H:%M:%S%z', '2009-01-14T06:20:59+0000'),
+# ('%l', '-'),
+# ('%>s', '200'),
+# ('%h', '163.1.2.86'),
+# ('%A:%p', '163.1.3.25:80'),
+# ('%{User-Agent}i', 'check_http/1.96 (nagios-plugins 1.4.5)'),
+# ('%b', '207'),
+# ('%{Referer}i', '-'),
+# ('%u', '-'),
+# ('%v', 'media.podcasts.ox.ac.uk'),
+# ('%r', 'GET / HTTP/1.0')]
  
  
-data.get('%{User-Agent}i')
-'check_http/1.96 (nagios-plugins 1.4.5)'
-'''
+# data.get('%{User-Agent}i')
+# 'check_http/1.96 (nagios-plugins 1.4.5)'
+
             print "Import finished\n\n"
 
 
-'''
-Reverse DNS====
-from dns import resolver,reversename
-addr=reversename.from_address("192.168.0.1")
-str(resolver.query(addr,"PTR")[0])
-'''
+# Reverse DNS====
+# from dns import resolver,reversename
+# addr=reversename.from_address("192.168.0.1")
+# str(resolver.query(addr,"PTR")[0])
+
     def _ip_to_domainname(ipaddress):
         return "www.%s.com" % ipaddress
