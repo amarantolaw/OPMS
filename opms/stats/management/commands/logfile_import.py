b19df3f6767503f24b56c960af699ca794d91602
Carl Marshall <carl.marshall@oucs.ox.ac.uk>
2011-02-04 17:52:39 +0000

    Slight cleanup to excel_import. Initial draft of logfile_import. Temp file in place for import of GeoIP data.

---
+++ opms/stats/management/commands/logfile_import.py
@@ -0,0 +1,68 @@
+# Import script for Apache Logfiles from media servers
+# Author: Carl Marshall
+# Last Edited: 4-2-2011
+
+from django.core.management.base import BaseCommand, CommandError
+from opms.stats.models import *
+import apachelog, time, sys
+from dns import resolver,reversename
+        
+class Command(BaseCommand):
+    args = '<spreadsheet.xls>'
+    help = 'Imports the contents of the specified spreadsheet into the database'
+
+    def handle(self, *args, **options):
+'''        
+Locate file
+Open file
+Read line by line
+  parse line
+  store in relevant fields
+  do associated lookups and stores
+'''
+        for filename in args:
+            # Some basic checking
+            if filename.endswith('.gz'):
+               raise CommandError("This file is still compressed. Uncompress and try again.\n\n")
+               # sys.exit(1)
+            else:
+               print "################  Beginning IMPORT from", filename
+        
+            # Assume mpoau logfiles
+            format = r'%Y-%m-%dT%H:%M:%S%z %v %A:%p %h %l %u \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"'
+            p = apachelog.parser(format)
+            
+            log = open(filename)
+            
+            for line in log:
+                print p.parse(line)
+                
+'''
+data.items()    
+[('%Y-%m-%dT%H:%M:%S%z', '2009-01-14T06:20:59+0000'),
+ ('%l', '-'),
+ ('%>s', '200'),
+ ('%h', '163.1.2.86'),
+ ('%A:%p', '163.1.3.25:80'),
+ ('%{User-Agent}i', 'check_http/1.96 (nagios-plugins 1.4.5)'),
+ ('%b', '207'),
+ ('%{Referer}i', '-'),
+ ('%u', '-'),
+ ('%v', 'media.podcasts.ox.ac.uk'),
+ ('%r', 'GET / HTTP/1.0')]
+ 
+ 
+data.get('%{User-Agent}i')
+'check_http/1.96 (nagios-plugins 1.4.5)'
+'''
+            print "Import finished\n\n"
+
+
+'''
+Reverse DNS====
+from dns import resolver,reversename
+addr=reversename.from_address("192.168.0.1")
+str(resolver.query(addr,"PTR")[0])
+'''
+    def _ip_to_domainname(ipaddress):
+        return "www.%s.com" % ipaddress
