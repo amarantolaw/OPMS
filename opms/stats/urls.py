e68eac93f283016f56c1265f0145d85bacd690ab
Carl Marshall <carl.marshall@oucs.ox.ac.uk>
2011-04-15 17:55:00 +0100

    hide the broken urls

--- opms/stats/urls.py
+++ opms/stats/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('stats.views',
     # These URLS will all change!
-    (r'^report/pr/item/(?P<guid>.+)', 'pr_report3'),
-    (r'^report/pr/partial-guid/(?P<partial_guid>.+)', 'pr_report2'),
-    (r'^report/pr/(?P<sort_by>.*)', 'pr_report1'),
+    #(r'^report/pr/item/(?P<guid>.+)', 'pr_report3'),
+    #(r'^report/pr/partial-guid/(?P<partial_guid>.+)', 'pr_report2'),
+    #(r'^report/pr/(?P<sort_by>.*)', 'pr_report1'),
     (r'^report/summary/$', 'summary_index'),
     (r'^$', 'index'),
 )
