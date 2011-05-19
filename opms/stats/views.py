1daf52c2a07fc1b21072c4322e9c87fff8326b85
Carl Marshall <carl.marshall@oucs.ox.ac.uk>
2011-05-19 17:21:03 +0100

    first test of matplotlib

--- opms/stats/views.py
+++ opms/stats/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render_to_response
 from django.http import Http404, HttpResponse
 from stats.models import Summary
 
+from pylab import figure, axes, pie, title
+from matplotlib import pyplot
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+
 
 # Default Stats module homepage
 def index(request):
@@ -20,3 +24,20 @@ def summary_weekof(request):
     "Show the results for a given week"
     return HttpResponse("Hello World. You're at the  Week of Results page.")
 
+
+
+def graph_apple_summary_totals(request):
+    f = figure(figsize=(6,6))
+    ax = axes([0.1, 0.1, 0.8, 0.8])
+    labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+    fracs = [15,30,45, 10]
+    explode=(0, 0.05, 0, 0)
+    pie(fracs, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True)
+    title('Raining Hogs and Dogs', bbox={'facecolor':'0.8', 'pad':5})
+
+    canvas = FigureCanvasAgg(f)    
+    response = HttpResponse(content_type='image/png')
+    canvas.print_png(response)
+    
+    pyplot.close(f)
+    return response
