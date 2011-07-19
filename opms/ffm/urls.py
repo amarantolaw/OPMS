be4cd4a6b95ea42f6fb2e57d54cfeeb5a42572ff
Carl Marshall <carl.marshall@oucs.ox.ac.uk>
2011-07-19 14:36:05 +0100

    Initial work on a Feed viewer and templates

--- opms/ffm/urls.py
+++ opms/ffm/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('ffm.views',
-    (r'^feeds/item/(?P<item_id>\d+)$', 'item_detail'),
+    (r'^items/(?P<item_id>\d+)$', 'item_detail'),
+    (r'^items/$', 'summary_items'),
+
     (r'^feeds/(?P<feed_id>\d+)$', 'feed_detail'),
     (r'^feeds/$', 'summary_feeds'),
+
+    (r'^people/(?P<person_id>\d+)$', 'person_detail'),
+    (r'^people/$', 'summary_people'),
 
     (r'^$', 'index'),
 )
--- opms/ffm/views.py
+++ opms/ffm/views.py
@@ -20,6 +20,23 @@ def feed_detail(request, feed_id):
     return HttpResponse("Hello World. You're at the FEED (" + str(feed_id) + ") DETAIL page.")
 
 
+def summary_items(request):
+    "List the Items and their associated files"
+    summary_data = []
+    return render_to_response('ffm/items.html', {'summary_data': summary_data,})
+
+
 def item_detail(request, item_id):
     "Show the results for a given item"
     return HttpResponse("Hello World. You're at the ITEM (" + str(item_id) + ") DETAIL page.")
+
+
+def summary_people(request):
+    "List the People and their associated Feeds and Items"
+    summary_data = []
+    return render_to_response('ffm/people.html', {'summary_data': summary_data,})
+
+
+def person_detail(request, person_id):
+    "Show the results for a given person"
+    return HttpResponse("Hello World. You're at the PERSON (" + str(person_id) + ") DETAIL page.")
