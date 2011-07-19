f401069aa1853a3fef8568a1fc43e961513f4cb8
Carl Marshall <carl.marshall@oucs.ox.ac.uk>
2011-07-19 14:21:38 +0100

    Initial work on a Feed viewer and templates

--- opms/ffm/urls.py
+++ opms/ffm/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('ffm.views',
-    (r'^feeds/item/(?P<item_id>.*)$', 'item_detail'),
-    (r'^feeds/(?P<feed_id>.*)$', 'feed_detail'),
+    (r'^feeds/item/(?P<item_id>\d+)$', 'item_detail'),
+    (r'^feeds/(?P<feed_id>\d+)$', 'feed_detail'),
     (r'^feeds/$', 'summary_feeds'),
 
     (r'^$', 'index'),
