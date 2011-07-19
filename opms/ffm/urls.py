436ac1dee433982be3384ef8f55755ebe2318e0b
Carl Marshall <carl.marshall@oucs.ox.ac.uk>
2011-07-19 14:15:17 +0100

    Initial work on a Feed viewer and templates

--- opms/ffm/urls.py
+++ opms/ffm/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('ffm.views',
+    (r'^feeds/item/(?P<item_id>.*)$', 'item_detail'),
+    (r'^feeds/(?P<feed_id>.*)$', 'feed_detail'),
+    (r'^feeds/$', 'summary_feeds'),
+
     (r'^$', 'index'),
 )
--- opms/ffm/views.py
+++ opms/ffm/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render_to_response
 from django.http import Http404, HttpResponse
+from ffm.models import *
 
 
 # Default FFM module homepage
 def index(request):
     # return HttpResponse("Hello World. You're at the OPMS:FFM Homepage.")
     return render_to_response('ffm/base.html', {})
-    #Comment test part 3
+
+
+def summary_feeds(request):
+    "List the Feedgroups and their associated feeds"
+    summary_data = []
+    return render_to_response('ffm/feedgroups.html', {'summary_data': summary_data,})
+
+
+def feed_detail(request, feed_id):
+    "Show the results for a given feed"
+    return HttpResponse("Hello World. You're at the FEED (" + str(feed_id) + ") DETAIL page.")
+
+
+def item_detail(request, item_id):
+    "Show the results for a given item"
+    return HttpResponse("Hello World. You're at the ITEM (" + str(item_id) + ") DETAIL page.")
