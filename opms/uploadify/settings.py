cf22788868341e514d894599d99c435e947af1e0
Carl Marshall <carl.marshall@oucs.ox.ac.uk>
2011-10-21 15:25:27 +0100

    Adding a test file upload system. Debugging.

--- opms/uploadify/settings.py
+++ opms/uploadify/settings.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 
 # Uploadify root folder path, relative to STATIC_ROOT
-UPLOADIFY_PATH = settings.UPLOADIFY_PATH = getattr(settings, 'UPLOADIFY_PATH', '%s%s' % (STATIC_URL, 'uploadify/'))
+UPLOADIFY_PATH = settings.UPLOADIFY_PATH = getattr(settings, 'UPLOADIFY_PATH', '%s%s' % ('STATIC_URL', 'uploadify/'))
 
 # Upload path that files are sent to
-UPLOADIFY_UPLOAD_PATH = settings.UPLOADIFY_PATH = getattr(settings, 'UPLOADIFY_UPLOAD_PATH', '%s%s' % (MEDIA_ROOT, 'uploads/'))
+UPLOADIFY_UPLOAD_PATH = settings.UPLOADIFY_PATH = getattr(settings, 'UPLOADIFY_UPLOAD_PATH', '%s%s' % ('MEDIA_ROOT', 'uploads/'))
