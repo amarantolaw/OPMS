1077e9c4b48cfa60385a94d6afad395dcc3f70c1
Carl Marshall <carl.marshall@oucs.ox.ac.uk>
2011-12-01 20:24:57 +0000

    Fixing OPMS to use staticfiles correctly (so that I can get the Django admin interface to work)

--- opms/settings_default.py
+++ opms/settings_default.py
@@ -73,12 +73,13 @@ TEMPLATE_LOADERS = (
 
 # New for Django 1.3?
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.media',
-    'django.core.context_processors.static',
-    'django.contrib.auth.context_processors.auth',
-    'django.contrib.messages.context_processors.messages',
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages"
 )
 
 MIDDLEWARE_CLASSES = (
