ec306d965bf1dccd8457d570d1237cc3a3f6e67f
Carl Marshall <carl.marshall@oucs.ox.ac.uk>
2011-12-05 17:44:58 +0000

    Attempting to implements template tags in Stats to help with navigation and other common elements

---
+++ opms/stats/templatetags/stats_tags.py
@@ -0,0 +1,27 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.tag(name="submenu-stats")
+def submenu(parser, current_section=0):
+    menu_html = u'''<h3>Sub sections for Statistics</h3>
+    <ul class="level-2">'''
+    if int(current_section) == 1:
+        menu_html += '<li><span class="youarehere">URL Monitoring</span></li>\n'
+    else:
+        menu_html += '<li><a href="/stats/report/urlmonitoring/">URL Monitoring</a></li>\n'
+    if int(current_section) == 2:
+        menu_html += '<li><span class="youarehere">Summary</span></li>\n'
+    else:
+        menu_html += '<li><a href="/stats/report/summary/">Summary</a></li>\n'
+    if int(current_section) == 3:
+        menu_html += '<li><span class="youarehere">Feeds</span></li>\n'
+    else:
+        menu_html += '<li><a href="/stats/report/summary/feeds/">Feeds</a></li>\n'
+    if int(current_section) == 4:
+        menu_html += '<li><span class="youarehere">Contributors</span></li>\n'
+    else:
+        menu_html += '<li><a href="/stats/report/authors/">Contributors</a></li>\n'
+    menu_html += '</ul>'
+    return mark_safe(menu_html)
