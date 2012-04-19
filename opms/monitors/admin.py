4dd28bdcc4d0ca894e1d1e0cd4b2ef5739770a56
Carl Marshall <carl.marshall@oucs.ox.ac.uk>
2012-04-19 15:28:21 +0100

    Created Monitors app and moved code for URL scanning and iTunes Store browsing to new app. Debugging NameError on admin for monitors app.

--- opms/monitors/admin.py
+++ opms/monitors/admin.py
@@ -3,14 +3,14 @@ from django.contrib import admin
 
 class URLMonitorTargetAdmin(admin.ModelAdmin):
     list_display = ('url', 'active')
-    list_filter = ['active']
+    list_filter = ('active')
     
 admin.site.register(URLMonitorTarget, URLMonitorTargetAdmin)
 
 
 class URLMonitorTaskAdmin(admin.ModelAdmin):
     list_display = ('comment', 'completed')
-    list_filter = ['completed']
+    list_filter = ('completed')
 
 admin.site.register(URLMonitorTask, URLMonitorTaskAdmin)
 
