e9a977bbd8305840609dc79aa39dffb4d1d3b604
Carl Marshall <carl.marshall@oucs.ox.ac.uk>
2012-04-19 16:25:06 +0100

    Created Monitors app and moved code for URL scanning and iTunes Store browsing to new app. Debugging NameError on admin for monitors app.

--- opms/urls.py
+++ opms/urls.py
@@ -3,11 +3,11 @@ from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
