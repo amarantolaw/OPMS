37b6796fdafdecb30314a0d1435ba2d0ba0c57c3
Carl Marshall <carl.marshall@oucs.ox.ac.uk>
2012-05-02 16:36:07 +0100

    Migrating to using Bootstrap from Twitter. Initial template test and setup...
    Subclassing the sitetree templates to use the Bootstrap code.

--- opms/templates/static/js/bootstrap-dropdown.js
+++ opms/templates/static/js/bootstrap-dropdown.js
@@ -91,6 +91,7 @@
    * =================================== */
 
   $(function () {
+    alert("Hello1")
     $('html').on('click.dropdown.data-api', clearMenus)
     $('body')
       .on('click.dropdown', '.dropdown form', function (e) { e.stopPropagation() })
