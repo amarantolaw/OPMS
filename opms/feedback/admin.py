b45dd37c5ee42cf5fa705568a68ee7558f38db94
sjlegg <sjlegg@gmail.com>
2012-07-18 14:33:03 +0100

    Fix CSS classes which got broken by previous commit.

--- opms/feedback/admin.py
+++ opms/feedback/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from feedback.models import Traffic, Comment, Event, Metric, Category
+from django.forms.widgets import Select
 import datetime
 
 def moderate(modeladmin, request, queryset):
@@ -14,6 +15,17 @@ def unmoderate(modeladmin, request, queryset):
     queryset.update(dt_moderated=None)
     queryset.update(moderator=None)
 unmoderate.short_description = "Reject feedback"
+
+class SelectSwitcher(Select):
+    def render(self, name, value, attrs=None, choices=()):
+        from django.utils.html import escape
+        self.choices = (u'choice1',u'choice2')
+        if value is None: value = ''
+        output = u"<select name=\"" + name + u"\">"
+        for choice in self.choices:
+                output += u"<option value=\"" + choice + u"\">" + choice + u"</option>"
+        output += u"</select>"
+        return output
 
 class TrafficInline(admin.TabularInline):
     model = Traffic
@@ -29,6 +41,10 @@ class EventInline(admin.StackedInline):
 
 class MetricAdmin(admin.ModelAdmin):
     inlines = [TrafficInline]
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name == 'description':
+            kwargs['widget'] = SelectSwitcher
+        return super(MetricAdmin, self).formfield_for_dbfield(db_field, **kwargs)
 
 class CategoryAdmin(admin.ModelAdmin):
     inlines = [CommentInline, EventInline]
