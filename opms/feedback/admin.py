ee5ad3978db3cdbc0c04cc36b7b68a6fb6a405b9
sjlegg <sjlegg@gmail.com>
2012-07-18 15:42:34 +0100

    Clean bad code out of admin.py.

--- opms/feedback/admin.py
+++ opms/feedback/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.conf import settings
 from django.contrib.auth.models import User
 from feedback.models import Traffic, Comment, Event, Metric, Category
 from django.forms.widgets import Select
+from django.utils.encoding import force_unicode
 import datetime
 
 def moderate(modeladmin, request, queryset):
@@ -15,17 +17,6 @@ def unmoderate(modeladmin, request, queryset):
     queryset.update(dt_moderated=None)
     queryset.update(moderator=None)
 unmoderate.short_description = "Reject feedback"
-
-class SelectSwitcher(Select):
-    def render(self, name, value, attrs=None, choices=()):
-        from django.utils.html import escape
-        self.choices = (u'choice1',u'choice2')
-        if value is None: value = ''
-        output = u"<select name=\"" + name + u"\">"
-        for choice in self.choices:
-                output += u"<option value=\"" + choice + u"\">" + choice + u"</option>"
-        output += u"</select>"
-        return output
 
 class TrafficInline(admin.TabularInline):
     model = Traffic
@@ -41,10 +32,6 @@ class EventInline(admin.StackedInline):
 
 class MetricAdmin(admin.ModelAdmin):
     inlines = [TrafficInline]
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        if db_field.name == 'description':
-            kwargs['widget'] = SelectSwitcher
-        return super(MetricAdmin, self).formfield_for_dbfield(db_field, **kwargs)
 
 class CategoryAdmin(admin.ModelAdmin):
     inlines = [CommentInline, EventInline]
