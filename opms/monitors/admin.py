1af380dfbd144c8853038cfd60e349cf8e39ec48
sjlegg <sjlegg@gmail.com>
2012-07-24 16:21:52 +0100

    Speed the admin up to a usable degree of missing information.

--- opms/monitors/admin.py
+++ opms/monitors/admin.py
@@ -14,20 +14,23 @@ class URLMonitorURLAdmin(admin.ModelAdmin):
 admin.site.register(URLMonitorURL, URLMonitorURLAdmin)
 
 admin.site.register(ItuInstitution)
-admin.site.register(ItuGenre)
 admin.site.register(ItuComment)
 
-class ItuCollectionChartScanInline(admin.StackedInline):
+
+class ItuCollectionChartScanInline(admin.TabularInline):
     model = ItuCollectionChartScan
     extra = 0
+    fields = ('date','position')
 
-class ItuItemChartScanInline(admin.StackedInline):
+class ItuItemChartScanInline(admin.TabularInline):
     model = ItuItemChartScan
     extra = 0
+    fields = ('date','position')
 
 class ItuCollectionHistoricalInline(admin.StackedInline):
     model = ItuCollectionHistorical
     extra = 0
+    fields = ('name','itu_id','img170','url','language','last_modified','contains_movies','version','scanlog','genre','institution')
 
 class ItuCollectionAdmin(admin.ModelAdmin):
     inlines = [ItuCollectionChartScanInline,ItuCollectionHistoricalInline]
@@ -37,6 +40,7 @@ admin.site.register(ItuCollection, ItuCollectionAdmin)
 class ItuItemHistoricalInline(admin.StackedInline):
     model = ItuItemHistorical
     extra = 0
+    fields = ('name','itu_id','url','artist_name','description','duration','explicit','feed_url','file_extension','kind','long_description','playlist_id','playlist_name','popularity','preview_length','preview_url','rank','release_date','version','institution','genre','scanlog')
 
 class ItuItemAdmin(admin.ModelAdmin):
     inlines = [ItuItemChartScanInline,ItuItemHistoricalInline]
@@ -46,10 +50,12 @@ admin.site.register(ItuItem, ItuItemAdmin)
 class ItuRatingInline(admin.TabularInline):
     model = ItuRating
     extra = 0
+    fields = ('stars','count')
 
 class ItuCommentInline(admin.StackedInline):
     model = ItuComment
     extra = 0
+    fields = ('date','detail','stars','source')
 
 class ItuCollectionHistoricalAdmin(admin.ModelAdmin):
     inlines = [ItuRatingInline,ItuCommentInline]
@@ -65,3 +71,8 @@ class ItuScanLogAdmin(admin.ModelAdmin):
     inlines = [ItuCollectionChartScanInline,ItuItemChartScanInline]
 
 admin.site.register(ItuScanLog, ItuScanLogAdmin)
+
+class ItuGenreAdmin(admin.ModelAdmin):
+    inlines = [ItuCollectionHistoricalInline]
+
+admin.site.register(ItuGenre, ItuGenreAdmin)
